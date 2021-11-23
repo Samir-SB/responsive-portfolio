ab5f5604d061b678f417f130d51d44ba67bbc24e
samir-sb <samir.belhadi@univ-batna2.dz>
2021-11-23 13:14:05 +0100

    remove log on console

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,10 +1,10 @@
-console.log('start server..');
+
 var btnMenu = document.querySelector('.btn-menu');
 var openIcon = btnMenu.querySelector('.icon-menu');
 var closeIcon = btnMenu.querySelector('.icon-close');
 
 var navbar = document.getElementById('main-navbar');
-console.log(closeIcon);
+
 btnMenu.addEventListener('click', function () {
   navbar.classList.toggle('collapsed-lg');
   openIcon.classList.toggle('hidden');
